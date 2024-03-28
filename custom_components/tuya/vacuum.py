ecb0b800c1d2b6afb285cd33dacb18a05d9cbba9
Guilherme Lirio Tomasi de Oliveira <guilhermelirio@gmail.com>
2024-03-28 02:55:08 -0300

    Update vacuum.py

--- custom_components/tuya/vacuum.py
+++ custom_components/tuya/vacuum.py
@@ -161,10 +161,10 @@ class TuyaVacuumEntity(TuyaEntity, StateVacuumEntity):
     def state(self) -> str | None:
         """Return Tuya vacuum device state."""
         if self.device.status.get(DPCode.PAUSE) and not (
-            self.device.status.get(DPCode.ROBOT_STATE)
+            self.device.status.get(DPCode.STATUS)
         ):
             return STATE_PAUSED
-        if not (status := self.device.status.get(DPCode.ROBOT_STATE)):
+        if not (status := self.device.status.get(DPCode.STATUS)):
             return None
         return TUYA_STATUS_TO_HA.get(status)
 
